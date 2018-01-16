4c469c0e6c45909f7f3fc5d1fcfaf8e3433fa040
coderman64 <coderman64@users.noreply.github.com>
2018-01-16 10:31:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var images = [
+    ["1-l.jpg","1-r.jpg"],
+    ["chair1_l.JPG","chair1_r.JPG"],
+    ["DrD_r.JPG","DrD_l.JPG"]
+];
+
+var l_eye = document.getElementById("lefteye1");
+var r_eye = document.getElementById("righteye1");
+var index = 0;
+var x = document.createElement("img");
+x.src = "res/"+images[(index+1)%images.length][0];
+
+var interval = window.setInterval(changeImage,10000);
+
+function changeImage(){
+    console.log("changing");
+    index++;
+    index = index%images.length;
+    
+    l_eye.setAttribute("src","res/"+images[index][0]);
+    l_eye.setAttribute("width",x.naturalWidth/900);
+    l_eye.setAttribute("height",x.naturalHeight/900);
+    r_eye.setAttribute("src","res/"+images[index][1]);
+    r_eye.setAttribute("width",x.naturalWidth);
+    r_eye.setAttribute("height",x.naturalHeight);
+    console.log(x.naturalWidth);
+    console.log(x.naturalHeight);
+    console.log("res/"+images[index][0])
+    console.log("res/"+images[index][1]);
+    console.log("changed");
+    x.src = "res/"+images[(index+1)%images.length][0];
+}
